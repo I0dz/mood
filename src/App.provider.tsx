d4ae9d7229bed3248d1eeaa8d05decab9b745e68
Loic Laudet <dlodz.me@gmail.com>
2022-03-14 17:05:47 +0100

    feat: save and get data using async storage

--- src/App.provider.tsx
+++ src/App.provider.tsx
@@ -1,6 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { MoodOptionWithTimeStamp } from './types';
+
+const storageKey = '@mood_tracker';
+interface AppData {
+    moods: MoodOptionWithTimeStamp[];
+}
+
+async function storeData(value: AppData) {
+    const jsonValue = JSON.stringify(value);
+    try {
+        await AsyncStorage.setItem(storageKey, jsonValue);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+async function getData(): Promise<AppData | null> {
+    try {
+        const stringifiedJsonValue = await AsyncStorage.getItem(storageKey);
+        return stringifiedJsonValue !== null
+            ? JSON.parse(stringifiedJsonValue)
+            : null;
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+}
 
 interface AppContextProps {
     moods: MoodOptionWithTimeStamp[];
@@ -10,12 +38,21 @@ interface AppContextProps {
 }
 const defaultValue = { moods: [], handleSelectedMood: () => {} };
 const AppContext = React.createContext<AppContextProps>(defaultValue);
-export const useAppContext = () => React.useContext(AppContext);
 
 export const AppProvider: React.FC = function ({ children }) {
     const [chosenMoods, setChosenMoods] = useState<MoodOptionWithTimeStamp[]>(
         [],
     );
+
+    useEffect(() => {
+        (async function getDataFromStorage() {
+            const data = await getData();
+            if (data) {
+                setChosenMoods(data?.moods);
+            }
+        })();
+    }, []);
+
     return (
         <AppContext.Provider
             value={{ moods: chosenMoods, handleSelectedMood: addMood }}>
@@ -24,6 +61,10 @@ export const AppProvider: React.FC = function ({ children }) {
     );
 
     function addMood(moodOptionWithTimeStamp: MoodOptionWithTimeStamp) {
-        setChosenMoods([...chosenMoods, moodOptionWithTimeStamp]);
+        const moodList = [...chosenMoods, moodOptionWithTimeStamp];
+        setChosenMoods(moodList);
+        storeData({ moods: moodList });
     }
 };
+
+export const useAppContext = () => React.useContext(AppContext);
